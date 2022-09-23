dd23f9eeaf2efd84ee03a6a4a41239418456a873
cdarko <carolinasnd2@hotmail.com>
2022-09-22 20:29:02 -0500

    Subida preentrega test

---
+++ js/events.js
@@ -0,0 +1,97 @@
+//Inputs del form
+ 
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const promo = document.querySelector('#promo');
+const botonPagar = document.querySelector('#botonPagar');
+
+
+
+//Estructura para los paquetes del evento
+class paquete {
+    constructor(titulo, caracteristicas, precio){
+        this.titulo = titulo;
+        this.caracteristicas = caracteristicas;
+        this.precio = precio;
+    }
+};
+
+
+//Nuevos paquetes
+const paquete1 = new paquete('Golden Pass', 'Entrada al evento, hospedaje en el destino, comida, bebidas, playera, vaso y parche conmemorativos.', '3000');
+const paquete2 = new paquete('Silver Pass', 'Entrada al evento, hospedaje en el destino, comida y bebidas', '2000');
+
+
+//Array de paquetes
+const paquetes = [];
+
+//Se agregan los paquetes nuevos al array
+paquetes.push(paquete1);
+paquetes.push(paquete2);
+
+
+
+//Contenido del div para motrar el precio del paquete seleccionado
+function mostrarPrecio(paquete){
+
+    const contenedorDePrecios = document.getElementById("contenedor-precio");
+    contenedorDePrecios.innerHTML = "";
+  
+    contenedorDePrecios.innerHTML = `
+      Precio: $ ${paquete.precio} 
+    `;
+  }
+
+
+//Se crea el boton "Seleccionar" y al dar clic muestra el precio con base en el paquete seleccionado
+function crearBotonSeleccionar(paquete){
+  const button = document.createElement("button");
+  button.innerText = "Seleccionar";
+  button.addEventListener("click", () => {
+      mostrarPrecio(paquete);
+    })
+  return button;
+}
+
+
+
+//Crea un div en el que se muestran las características de los paquetes junto con el botón seleccionar
+function mostrarPaquetes(paquetes) {
+    const contenedorDePaquetes = document.getElementById('contenedor-de-paquetes');
+    contenedorDePaquetes.innerHTML = "";
+
+    paquetes.forEach(paquete => {
+        const divPaquete = document.createElement("div"); // <div></div>
+        divPaquete.classList.add('paquete');// <div class='paquete'></div>
+        divPaquete.innerHTML = `
+        <h3>${paquete.titulo}</h3>
+        <p>${paquete.caracteristicas}</p>
+        <p>$ ${paquete.precio}</p>
+        `;
+        
+
+     // ==========
+        const botonSeleccionar = crearBotonSeleccionar(paquete)
+
+
+     // ==========        
+        divPaquete.appendChild(botonSeleccionar);
+        contenedorDePaquetes.append(divPaquete);
+
+    });    
+}
+
+
+//Publica la sección de paquetes para el evento
+mostrarPaquetes(paquetes);
+
+
+//Crea un div en el que se muestran las características de los paquetes junto con el botón seleccionar
+botonPagar.addEventListener('click', () => {
+  console.log(username.value);
+  console.log(email.value);
+  console.log(promo.value);
+
+})
